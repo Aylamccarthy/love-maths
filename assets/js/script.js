77b0e07d2ac0463f9e56ae367fcbfaefa8c1beb7
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-01-27 21:45:43 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calcuteCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+
+}
+
+function displayMultiplyQuestion(){
+    
+}
