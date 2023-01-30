11fbea1ebb0acc7854c41d2d94395e17c323f747
Aylamccarthy <aylamarmol@yahoo.co.uk>
2023-01-30 13:12:20 +0000

    Complete the division game code

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,7 +46,7 @@ function runGame(gameType){
   } else if (gameType === "subtract") {
     displaySubtractQuestion(num1, num2);
   }else if (gameType === "division") {
-    displayDivisionGame(num1, num2);
+    displayDivisionQuestion(num1, num2);
   }
 
   else {
@@ -127,7 +127,7 @@ function displayAdditionQuestion(operand1, operand2){
 
 function displaySubtractQuestion(operand1, operand2){
   document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-  document.getElementById('operand2').textContent = operand2 > operand1 ? operand2 : operand1;
+  document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
   document.getElementById('operator').textContent = "-";
 
 
@@ -140,9 +140,9 @@ function displayMultiplyQuestion(operand1, operand2){
   document.getElementById('operator').textContent = "x";
 }
 
-function displayDivisionGame(operand1, operand2) {
-  document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-  document.getElementById('operand2').textContent = operand2 > operand1 ? operand2 : operand1;
+function displayDivisionQuestion(operand1, operand2) {
+  document.getElementById('operand1').textContent = operand1 * operand2;
+  document.getElementById('operand2').textContent = operand2 ;
   document.getElementById('operator').textContent = "/";
 
 }
